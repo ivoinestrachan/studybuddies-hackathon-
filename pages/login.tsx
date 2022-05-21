84a0177b7778b9f57a007343ab5aa5a379f79f95
Emily Cai <iac.ylime@gmail.com>
2022-05-21 16:01:46 -0700

    login v2

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,12 +1,20 @@
 import type { NextPage } from 'next'
 import styles from "../styles/login.module.css"
+import Image from 'next/image';
+import logo from "../assets/logo.svg"
 
-const Login: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <h1>login?</h1>
-    </div>
-  )
+function Login(): JSX.Element {
+    return (
+        <div className={styles.container}>
+            <div className={styles.headerImg}>
+                <Image src={logo} className={styles.logo} alt="logo" />
+            </div>
+            <div className={styles.box}>
+                <button className={styles.button}>Login</button>
+                <button className={styles.button}>Sign Up</button>
+            </div>
+        </div>
+    );
 }
 
 export default Login
