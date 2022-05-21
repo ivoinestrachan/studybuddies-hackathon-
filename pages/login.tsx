c8bd6408aae5feff172b96e2b950a5ef79c9103b
Emily Cai <iac.ylime@gmail.com>
2022-05-21 11:09:52 -0700

    login v1

---
+++ pages/login.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import styles from "../styles/login.module.css"
+
+const Login: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <h1>login?</h1>
+    </div>
+  )
+}
+
+export default Login
